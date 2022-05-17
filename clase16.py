4e8e8c36d8dbb3d4f1495bbece44c72955c72d53
santiagomarano <santiagomarano@gmail.com>
2022-05-16 23:03:36 -0300

    primer comit en rama master

--- clase16.py
+++ clase16.py
@@ -2,9 +2,10 @@
 #GIT --config user.name "el nombre"
 #GIT --config user.email mail@mail.com
 #GIT --config ver config
-#GIT config -
-#
-#
+#GIT config --list
+#-----------------------------------
+#GIT add (pasar de working directory a stanging area)
+#GIT commir -m "agregar comentario"
 #
 #
 #
