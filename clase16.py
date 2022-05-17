ed52a6318b5a3d13b10959f8caca4235dd1e616c
santiagomarano <santiagomarano@gmail.com>
2022-05-16 23:02:40 -0300

    primer comit en rama master

---
+++ clase16.py
@@ -0,0 +1,12 @@
+#GIT INIT  crear un repositorio (git init nuevo_repo)
+#GIT --config user.name "el nombre"
+#GIT --config user.email mail@mail.com
+#GIT --config ver config
+#GIT config -
+#
+#
+#
+#
+#
+#
+
